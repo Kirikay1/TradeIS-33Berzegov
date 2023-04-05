758cf7ec8cb2e22b5770a0e52c7c00ff48d41af5
Maksim <maksim.timoshin.045@gmail.com>
2023-04-05 10:53:46 +0300

    init: start project

---
+++ WpfAppTovarIS-33Berzegov/WpfAppTovarIS-33Berzegov/ViewModel/BaseVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTovarIS_33Berzegov.ViewModel
+{
+    internal class BaseVM
+    {
+    }
+}
